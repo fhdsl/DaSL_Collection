a45b8fe37a4b61b6f04f5f6e15cba52b2623d03e
avahoffman <avamariehoffman@gmail.com>
2022-08-08 11:54:00 -0400

    Fix another typo

--- scripts/query_collection.R
+++ scripts/query_collection.R
@@ -35,7 +35,7 @@ if (!(httr::http_error(req))) {
   # Read in and save data
   repo_dat <-
     jsonlite::fromJSON(httr::content(req, as = "text"), flatten = TRUE)
-  message(paste("...", repo_dat_1$total_count, " repositories detected."))
+  message(paste("...", repo_dat$total_count, " repositories detected."))
   
   # Modify the request results to get what we need
   repo_df <-
