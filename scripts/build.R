98d817cc9a15ffe9dac1e25fcaa8e32aa97310cf
avahoffman <avamariehoffman@gmail.com>
2024-01-30 21:12:47 -0500

    Update build.R

--- scripts/build.R
+++ scripts/build.R
@@ -5,7 +5,13 @@ print(getwd())
 #   output_file = "index.md",
 #   output_format = md_document(variant = "markdown_phpextra", preserve_yaml = TRUE)
 # )
-bookdown::render_book('index.Rmd', output_format = 'all')
+# bookdown::render_book('index.Rmd', output_format = 'all')
 file.copy(from = 'assets',
           to = 'docs/assets',
           overwrite = TRUE)
+rmarkdown::render(
+  input = 'index.Rmd',
+  output_file = "index.html",
+  output_format = html_document(output_dir = "docs"),
+  output_dir = "docs",
+)
