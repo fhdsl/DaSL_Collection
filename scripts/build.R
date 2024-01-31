5414fa96d2f19b5769bf574f95e08474663f5970
avahoffman <avamariehoffman@gmail.com>
2024-01-30 20:13:51 -0500

    Trying to go back to rmarkdown render

--- scripts/build.R
+++ scripts/build.R
@@ -5,7 +5,14 @@ print(getwd())
 #   output_file = "index.md",
 #   output_format = md_document(variant = "markdown_phpextra", preserve_yaml = TRUE)
 # )
-bookdown::render_book('index.Rmd', output_format = 'all')
-file.copy(from = 'assets',
-          to = 'docs/assets',
-          overwrite = TRUE)
+# bookdown::render_book('index.Rmd', output_format = 'all')
+# file.copy(from = 'assets',
+#           to = 'docs/assets',
+#           overwrite = TRUE)
+
+library(rmarkdown)
+print(getwd())
+render(
+  input = 'index.Rmd',
+  output_format = "html_document"
+)
