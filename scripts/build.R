f21c831559189bb11b45bb117ffd3effe873dfa2
avahoffman <avamariehoffman@gmail.com>
2024-01-30 22:26:17 -0500

    Some final cleanup before calling this one

--- scripts/build.R
+++ scripts/build.R
@@ -5,14 +5,15 @@ print(getwd())
 #   output_file = "index.md",
 #   output_format = md_document(variant = "markdown_phpextra", preserve_yaml = TRUE)
 # )
-# bookdown::render_book('index.Rmd', output_format = 'all')
+remotes::install_deps(dependencies = TRUE)
+bookdown::render_book('index.Rmd', output_format = 'all')
 file.copy(from = 'assets',
           to = 'docs/assets',
           overwrite = TRUE)
-install.packages(c("httpuv", "promises", "knitr"), repos = "https://cloud.r-project.org")
-rmarkdown::render(
-  input = 'index.Rmd',
-  output_file = "index.html",
-  output_format = rmarkdown::html_document(output_dir = "docs"),
-  output_dir = "docs",
-)
+# install.packages(c("httpuv", "promises", "knitr"), repos = "https://cloud.r-project.org")
+# rmarkdown::render(
+#   input = 'index.Rmd',
+#   output_file = "index.html",
+#   output_format = rmarkdown::html_document(output_dir = "docs"),
+#   output_dir = "docs",
+# )
