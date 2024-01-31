8d63b6724f6fbe9587cf8f7bf5b6c7bb933814e9
avahoffman <avamariehoffman@gmail.com>
2024-01-30 22:31:13 -0500

    remove deps

--- scripts/build.R
+++ scripts/build.R
@@ -5,7 +5,6 @@ print(getwd())
 #   output_file = "index.md",
 #   output_format = md_document(variant = "markdown_phpextra", preserve_yaml = TRUE)
 # )
-remotes::install_deps(dependencies = TRUE)
 bookdown::render_book('index.Rmd', output_format = 'all')
 file.copy(from = 'assets',
           to = 'docs/assets',
